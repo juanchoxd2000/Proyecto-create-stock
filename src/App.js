030069a655e99516779bf07e29d6b0f6f7e6cc76
Juan Petit <juan.jose.petit@hotmail.com>
2020-11-16 12:45:56 -0300

    TarjetaCliente creada con exito

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
 import './App.css';
-
+import TarjetaCliente from "./componentes/TarjetaCliente"
 import "./data/clientes"
 import clientes from './data/clientes';
 
-function TarjetaCliente (props) {
-  return (
-      <ul>
-          <li>
-              nombre={clientes[props].nombre}
-          </li>
-          <li>
-              edad={clientes[props].edad}
-          </li>
-          <li>
-              fechaIngreso={clientes[props].fechaIngreso}
-          </li>
-      </ul>
-  );
-}
 
 function App() {
   console.log(clientes)
--- src/componentes/TarjetaCliente.jsx
+++ src/componentes/TarjetaCliente.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 import clientes from "../data/clientes";
 
-function TarjetaCliente (props) {
+function TarjetaCliente() {
     return (
         <ul>
             <li>
-                nombre={clientes[props].nombre}
+                Nombre = {clientes[0].nombre}
             </li>
             <li>
-                edad={clientes[props].edad}
+                Edad = {clientes[0].edad}
             </li>
             <li>
-                fechaIngreso={clientes[props].fechaIngreso}
+                Fecha de ingreso = {clientes[0].fechaIngreso}
             </li>
         </ul>
     );
-}
+};
 
-export default "TarjetaCliente";
+export default TarjetaCliente;
